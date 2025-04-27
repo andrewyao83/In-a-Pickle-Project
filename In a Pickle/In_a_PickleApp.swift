60adaac0514109465800f9e737b02cf6c879bb24
Andrew <ayao8@uw.edu>
2025-04-26 23:27:03 -0700

    Initial Commit

---
+++ In a Pickle/In_a_PickleApp.swift
@@ -0,0 +1,17 @@
+//
+//  In_a_PickleApp.swift
+//  In a Pickle
+//
+//  Created by Andrew Yao on 4/26/25.
+//
+
+import SwiftUI
+
+@main
+struct In_a_PickleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ In a PickleTests/In_a_PickleTests.swift
@@ -0,0 +1,16 @@
+//
+//  In_a_PickleTests.swift
+//  In a PickleTests
+//
+//  Created by Andrew Yao on 4/26/25.
+//
+
+import Testing
+
+struct In_a_PickleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
